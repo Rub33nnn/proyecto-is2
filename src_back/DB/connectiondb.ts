b2917b792d9739f9369d8340d4f1dcc98f3d83f9
Rub33nnn <155687273+Rub33nnn@users.noreply.github.com>
2024-12-17 15:50:35 -0700

    Delete src_back directory

--- src_back/DB/connectiondb.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-//Aqui se hace la conexion a la BD
-const sequilize = new Sequelize('bd_appmensajeria', 'root', 'root', { //(Nombre de la BD, usuario, contraseña)
-    host: 'localhost', //Host
-    dialect: 'mysql' //el server que usamos para la bd en este caso mysql
-})
-
-export default sequilize
--- src_back/index.ts
+++
@@ -1,5 +0,0 @@
-import Server from "./models/server";
-import dotenv from 'dotenv'
-
-dotenv.config() //configurar las variables de ambiente
-const server = new Server()
